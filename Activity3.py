7496d15331e36cd9c705087f6b6ae73ec89531dc
dhavaniamin1331 <a.nirali266@gmail.com>
2025-01-29 19:17:24 +0200

    Actiity 3

---
+++ Activity3.py
@@ -0,0 +1,14 @@
+# Program to prnt number decrasing then increasing order 
+
+def incdec(n, num):
+    # Base case to return value when we reach our limit 
+    if(n<1 or n>num):
+        return
+    # Print decreasing first 
+    print(n)
+    incdec(n-1,num)
+    # Print increasing order 
+    print(n) 
+
+    n = int(input("Enter any number n : "))
+    incdec(n,n)
